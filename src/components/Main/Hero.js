6cf4c1e35303baa3037f34da43ffd6ab8e9018c0
Łukasz Zabuski <riraseth@gmail.com>
2021-06-14 20:47:00 +0200

    v.995

--- src/components/Main/Hero.js
+++ src/components/Main/Hero.js
@@ -1,13 +1,13 @@
 const Hero = () => {
   return (
-    <div className="hero">
+    <a href="https://www.google.com/" className="hero">
       <img
         src="https://source.unsplash.com/6-C0VRsagUw/800x500"
         alt=""
         className="hero__img"
       />
       <h1 className="hero__title">Lorem, ipsum dolor.</h1>
-    </div>
+    </a>
   );
 };
 
