561d93ad0fe7ddfce54dedfa92207c662cd0657c
Łukasz Zabuski <riraseth@gmail.com>
2021-06-15 18:51:24 +0200

    removed styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,7 @@
-import { useState, useEffect } from 'react';
 import Nav from './Nav';
 const Header = () => {
-  const [positionY, setPositionY] = useState(0);
-
-  const scroll = () => {
-    setPositionY(window.pageYOffset);
-  };
-
-  useEffect(() => {
-    const watchScroll = () => {
-      window.addEventListener('scroll', scroll);
-    };
-    watchScroll();
-    return () => window.removeEventListener('scroll', scroll);
-  }, []);
-  console.log(positionY);
   return (
-    <header
-      className={`header container ${positionY > 40 ? 'header--scrolled' : ''}`}
-    >
+    <header className="header container">
       <a href="https://www.google.com/" className="header__link">
         <img
           src="https://source.unsplash.com/6-C0VRsagUw/300x120"
--- src/components/Main/Hero.js
+++ src/components/Main/Hero.js
@@ -1,13 +1,13 @@
 const Hero = () => {
   return (
-    <div className="hero">
+    <a className="hero" href="https://www.google.com/">
       <img
         src="https://source.unsplash.com/6-C0VRsagUw/800x500"
         alt=""
         className="hero__img"
       />
       <h1 className="hero__title">Lorem, ipsum dolor.</h1>
-    </div>
+    </a>
   );
 };
 
